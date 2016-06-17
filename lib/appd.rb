c9c72f7092df9528408770c3cc592ec143793ac3
Morgan Showman <mshowman@squaremouth.com>
2016-06-17 14:54:27 -0400

    Add server option
    This commit adds a server option so a specific server can be used.

--- lib/appd.rb
+++ lib/appd.rb
@@ -2,7 +2,7 @@ require "appd/app"
 require "appd/version"
 
 module Appd
-  def self.exec(command)
-    super command
+  def self.exec(command, **options)
+    super "#{"source ~/.appd/#{options[:server]} && " if options[:server]}#{command}"
   end
 end
