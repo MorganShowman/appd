09d945520cc6cffaeb4d15182cb15002dfa32615
Morgan Showman <mshowman@squaremouth.com>
2016-06-16 22:27:38 -0400

    Remove unused docker_select method
    This commit removes the unused docker_select method.

--- lib/appd.rb
+++ lib/appd.rb
@@ -2,10 +2,6 @@ require "appd/app"
 require "appd/version"
 
 module Appd
-  def self.docker_select(server)
-    puts `source ~/.appd/#{server}`
-  end
-
   def self.exec(command)
     super command
   end
